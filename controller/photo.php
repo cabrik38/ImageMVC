69f6c11fb57d47525b49c58f60366793b66a0eae
dorian <cabrik.ben@gmail.com>
2018-11-08 14:45:12 +0100

    Deux fois la fonction likeAction

--- controller/photo.php
+++ controller/photo.php
@@ -354,26 +354,4 @@ class Photo {
             require_once("view/mainView.php");
         }
     }
-
-
-	/**
-	* Permet d'enregistrer un j'aime
-	*/
-	public function likeAction(){
-		if (isset($_GET["imgId"]) && is_numeric($_GET["imgId"])) {
-			$img = $this->imgDAO->getImage($_GET["imgId"]);
-			$notes = $img->getNotes();
-			if(isset($_GET["like"]) ){
-				if($_GET["like"] == "like"){
-					$img ->setNotes($notes + 1);
-				} else {
-					$img-> setNotes($notes - 1);
-				}
-			}
-			$this->imgDAO->saveLike($img);
-			$data = $this->getData($img);
-			$data["view"] = "photoView.php";
-			require_once("view/mainView.php");
-		}
-	}
 }
